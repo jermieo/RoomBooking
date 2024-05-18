d3fd789ca36275d9fc106fbd15e8921e227248dd
jermieo <jermieorex1798@gmail.com>
2024-05-18 15:12:24 +0530

    api change

--- src/Apis/Api.jsx
+++ src/Apis/Api.jsx
@@ -1,4 +1,5 @@
-export const host = "http://localhost:4000";
+// export const host = "http://localhost:4000";
+export const host = "https://roombookingbe.onrender.com";
 export const create = `${host}/api/users/create`;
 export const login = `${host}/api/users/login`;
 export const bookings = `${host}/api/users/bookings`;
